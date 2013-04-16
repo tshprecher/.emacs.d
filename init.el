901206f589e38e3f55cec94aa54d79377be42788
Tal Shprecher <tshprecher@twitter.com>
2013-04-16 16:41:24 -0700

    change init.el to add ruby-mode

--- init.el
+++ init.el
@@ -2,3 +2,8 @@
 (add-to-list 'load-path "~/.emacs.d/scala-mode/src")
 (require 'scala-mode-auto)
 
+;; ruby mode
+(add-to-list 'load-path "~/.emacs.d/ruby-mode")
+(require 'ruby-mode)
+(setq auto-mode-alist  (cons '(".rb$" . ruby-mode) auto-mode-alist))
+
