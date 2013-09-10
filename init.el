d4be36870d6d0c134c3023f3d1c2cafd14f961e8
Tal Shprecher <tshprecher@twitter.com>
2013-09-09 20:53:12 -0700

    change ecb-source-path to ~

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
 	     "~/.emacs.d/ecb/ecb-2.40-cedet-wrapper")
 (require 'ecb)
 (setq stack-trace-on-error t)
+
 (ecb-activate)
 (ecb-byte-compile)
 
@@ -57,7 +58,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.40"))
+ '(ecb-options-version "2.40")
+ '(ecb-source-path (quote (("~" "~")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
