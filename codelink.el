a8871c7b7d017103f6c0d7f8658ec4c5362e8a59
Tal Shprecher <tshprecher@twitter.com>
2015-02-25 00:21:08 -0800

    basic logic to dispatch to link, needs fleshing out and cleanup

--- codelink.el
+++ codelink.el
@@ -1,17 +1,39 @@
 ;; open browser to supported web ui view of file
 (require 'cl)
 
-;; ***** ABSTRACT REPO *****
+(setq cl-global-registry-list nil)
+
+;; Opens a link pointing to the line of the current file. If the file is registered
+;; under multiple repos, then the first one in the global registry list is used.
+;; TODO: handle branch logic
+
+(defun cl-goto-link ()
+  (interactive)
+  (let* ((filename (buffer-file-name (current-buffer)))
+         (repo (car
+                (delq nil (mapcar (lambda (r)
+                                 (if (not (string-prefix-p (cl-repo-dir r) filename))
+                                     nil
+                                   r))
+                                 cl-global-registry-list)))))
+    (if (not repo)
+        nil
+;; TODO: remove      (list filename (cl-repo-dir repo) (substring filename (length (cl-repo-dir repo)))))))
+      (cl-repo-goto-link
+       repo
+       (substring filename (length (cl-repo-dir repo)))
+       (line-number-at-pos)
+       nil))))
+
+;; abstract repo
 
 (defclass cl-repo ()
   ((name
     :initarg   :name
     :accessor  cl-repo-name)
-
    (server
     :initarg   :server
     :accessor  cl-repo-server)
-
    (dir
     :initarg   :dir
     :accessor  cl-repo-dir))
@@ -27,18 +49,14 @@
 
   "abstract definition of goto-link method")
 
-;; **********************************
-
-;; ********* CGIT REPO ************
-
+;; cgit repo
 (defclass cl-cgit-repo (cl-repo) ())
 
 (defun cl-make-cgit-repo (name server dir)
   (make-instance 'cl-cgit-repo :name name :server server :dir (expand-file-name dir)))
 
-
 ;; TODO: integrate branch
-(defmethod cl-cgit-repo-goto-link ((repo cl-cgit-repo) filename line branch)
+(defmethod cl-repo-goto-link ((repo cl-cgit-repo) filename line branch)
 ;; https://cgit.twitter.biz/twitter/tree/config/api/special_clients.yml#n59
   (browse-url
    (format
@@ -52,10 +70,17 @@
 
 ;; debugging zone
 
-(expand-file-name "/Applications/Google\\ Chrome.app/Contents/MacOS/")
-(setq twitter-repo (cl-make-cgit-repo "twitter" "cgit.twitter.biz" "~/workspace/twitter"))
-(cl-cgit-repo-goto-link twitter-repo "config/api/special_clients.yml" 12 nil)
 
-;; (expand-file-name "~")
+(setq twitter-repo (cl-make-cgit-repo "twitter" "cgit.twitter.biz" "~/workspace/twitter"))
+(setq test-repo (cl-make-cgit-repo "localemacs" "cgit.local" "~/.emacs.d/"))
+
+;;(cl-cgit-repo-goto-link twitter-repo "config/api/special_clients.yml" 12 nil)
+(setq cl-global-registry-list (list test-repo))
+
+(string-prefix-p "/Users/tshprecher/workspace/.emacs.d/" "/Users/tshprecher/.emacs.d/codelink.el")
+
+
+(cl-goto-link)
+;; (expand-file-name "/Applications/Google\\ Chrome.app/Contents/MacOS/")
 ;; (setq test-reg (cl-make-cgit-registry "name" "hostname" "dirs"))
 ;; (get-name test-reg)
